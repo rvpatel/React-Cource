8c56a9b3e0b623fd5b2e205b6fec25fb05d543c5
rvpatel <ravipatelb07272@gmail.com>
2021-10-06 06:11:54 +0530

    inlibe and internal css on H1 and H2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'ReactDOM';
+import './style.css';
+
+const heading = {
+  color: 'brown',
+  textAlign: 'center',
+  textTransform: 'capitalize',
+  fontWeight: 'bold',
+  textShadow: '0px 3px 5px #ffee9c',
+};
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1 style={heading}>Hello, Ravikumar</h1>
+      <h2
+        style={{
+          color: 'red',
+          textAlign: 'center',
+          textTransform: 'capitalize',
+          fontWeight: 'bold',
+          textShadow: '0px 3px 5px #ffee9c',
+        }}
+      >
+        Inline Css
+      </h2>
+      <p>Good Morning :)</p>
     </div>
   );
 }
