cc0c6dd9249f1e16079e53213fd68edbba5b3ce7
rvpatel <ravipatelb07272@gmail.com>
2021-10-31 06:13:20 +0530

    toggle code using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './style.css';
 
 export default function App() {
+  const [data, setData] = useState(false);
+  const [print, SetPrint] = useState('Ahmedabad');
+
+  function getData(val) {
+    SetPrint(val.target.value);
+    setData(val.target.value);
+  }
+
   return (
     <div>
-      <h1 className="heading">Hello, Ravikumar</h1>
-      <p>Good Morning :)</p>
+      <p>Input box Code :</p>
+      {data ? <h1>{data}</h1> : print}
+      <input name="city" type="text" onChange={getData} />
+      <button onClick={() => setData(!data)}>Toggle</button>
     </div>
   );
 }
