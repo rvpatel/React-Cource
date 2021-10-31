13acbeaa5e2eb055c8efa9ff351086a8bf2f3b12
rvpatel <ravipatelb07272@gmail.com>
2021-10-31 06:40:51 +0530

    from structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [tnc, setTnc] = useState(true);
+  const [favi, setfavi] = useState('');
+
+  function getFormdata(e) {
+    e.preventDefault();
+  }
   return (
     <div>
-      <h1 className="heading">Hello, Ravikumar</h1>
-      <p>Good Morning :)</p>
+      <h1>Social media Platform</h1>
+      <form onSelect={getFormdata}>
+        <input type="text" placeholder="Enter Name" name="name" value={name} />
+        <br />
+        <select
+          onChange={(e) => {
+            setfavi(e.target.value);
+          }}
+        >
+          <option value="">Select</option>
+          <option value="Facebook">Facebook</option>
+          <option value="Linked-in">Linked-in</option>
+        </select>
+        <br />
+        <input
+          type="checkbox"
+          placeholder="Term"
+          name="term"
+          onChange={(e) => setTnc(e.target.checked)}
+        />
+        Term & conditon
+        <br />
+        <button>Select</button>
+      </form>
     </div>
   );
 }
