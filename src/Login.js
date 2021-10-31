a2439f837a1bb9a8a7c84c0b6943d861bccfb4d9
rvpatel <ravipatelb07272@gmail.com>
2021-10-31 07:43:29 +0530

    From validation using package

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
+import validator from 'validator';
 
 function Login() {
-  const [User, setUser] = useState(' ');
-  const [Password, setPassword] = useState('');
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [pssErr, setPassErr] = useState(false);
+
   function LoginHandle(e) {
     e.preventDefault();
   }
 
+  function UserHandle(e) {
+    e.preventDefault();
+    let email = e.target.value;
+    if (validator.isEmail(email)) {
+      setEmailError('Valid Email :)');
+    } else {
+      setEmailError('Enter valid Email!');
+    }
+  }
+
+  function passHandle(e) {
+    let pass = e.target.value;
+    if (validator.isStrongPassword(pass)) {
+      setEmailError('Valid Pass :)');
+    } else {
+      setEmailError('Enter strong!');
+    }
+  }
+
   return (
-    <div>
+    <div style={{ backgroundColor: '#909090', padding: '20px' }}>
       <h1>Login</h1>
       <form onSubmit={LoginHandle}>
-        <input type="text" Placeholder="Enter Password" />
+        <input type="text" Placeholder="Enter Email ID" onChange={UserHandle} />
+        <input type="text" Placeholder="Enter Password" onChange={passHandle} />
+        <br />
+        {emailError}
+        <br />
+        {pssErr}
         <br />
         <br />
         <button type="submit">Login</button>
