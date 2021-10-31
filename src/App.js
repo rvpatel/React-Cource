fca5343890a26a301f7e2fef51965f5afb66c76b
rvpatel <ravipatelb07272@gmail.com>
2021-10-31 05:49:28 +0530

    Input box using useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './style.css';
 
 export default function App() {
+  const [data, setData] = useState(null);
+  const [print, SetPrint] = useState(false);
+
+  function getData(val) {
+    setData(val.target.value);
+  }
+
   return (
     <div>
-      <h1 className="heading">Hello, Ravikumar</h1>
-      <p>Good Morning :)</p>
+      <p>Input box Code :</p>
+      {print ? <h1>{data}</h1> : null}
+      <input name="city" type="text" onChange={getData} />
+      <button onClick={() => SetPrint(true)}>Set value</button>
     </div>
   );
 }
